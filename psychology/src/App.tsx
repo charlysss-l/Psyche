f42fe8c9b011d8d7b63a45bb6f2818a0f87f6cc4
charlysss-l <charlizemikaelanadela@gmail.com>
2024-10-20 11:57:41 +0800

    Psychology - fix navbar , sidebar, main content css

--- psychology/src/App.tsx
+++ psychology/src/App.tsx
@@ -1,27 +1,30 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
-import Sidebar from './components/Sidebar/Sidebar';  
+import Sidebar from './components/Sidebar/Sidebar';
 import Report from './app/Report/Report';
 import Test from './app/Test/Test';
 import User from './app/User/User';
-import Profile from './app/Profile/Profile';  
-import OMR from './app/OMR/OMR'; 
+import Profile from './app/Profile/Profile';
+import OMR from './app/OMR/OMR';
+import './App.css';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Sidebar /> {/* Sidebar */}
+        <Sidebar /> 
         <div className="main-content">
-          <Navbar /> {/* Navbar */}
-          <Routes>
-            <Route path="/" element={<Report />} />
-            <Route path="/test" element={<Test />} />
-            <Route path="/user" element={<User />} />
-            <Route path="/profile" element={<Profile />} /> 
-            <Route path="/omr" element={<OMR />} />
-          </Routes>
+          <Navbar /> 
+          <div className="content">
+            <Routes>
+              <Route path="/" element={<Report />} />
+              <Route path="/test" element={<Test />} />
+              <Route path="/user" element={<User />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/omr" element={<OMR />} />
+            </Routes>
+          </div>
         </div>
       </div>
     </Router>
