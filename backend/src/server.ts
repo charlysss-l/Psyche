c2d7058e3bb59554bee2602bd69852c750c95013
charlysss-l <charlizemikaelanadela@gmail.com>
2024-10-26 11:23:19 +0800

    MAIN - Create IQ Routes. EVERYTHING WORKS

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,9 +4,8 @@ import dotenv from 'dotenv';
 import connectDB from './config/db';
 import user16PFTestRoutes from './routes/user16PFTestRoutes';
 import userIQTestRoutes from './routes/UserIQTestRoutes';
-
 import Test16PFRoutes from './routes/Test16PFRoutes';
-
+import IQTestRoutes from './routes/IQTestRoutes';
 dotenv.config();
 
 const app: Application = express();
@@ -20,7 +19,7 @@ app.use(express.json());
 app.use('/api/user16pf', user16PFTestRoutes);   // 16PF test routes
 app.use('/api/useriq', userIQTestRoutes);
 app.use('/api/16pf', Test16PFRoutes);
-
+app.use('/api/IQtest', IQTestRoutes);
 
 const PORT = process.env.PORT || 5000;
 
