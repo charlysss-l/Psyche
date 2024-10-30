718308bae87fc4d6b76a9e2fa4be325fd39f83c1
Kel Soberano <kellsoberano@gmail.com>
2024-10-30 16:50:04 +0800

    backend - install firebase, multer. Createdconfig for firebase

---
+++ Backend/src/config/firebaseConfig.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import {getStorage} from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBWj1L7qdsRH4sFpE7q0CaoyL55KWMGRZI",
+  authDomain: "iqtestupload.firebaseapp.com",
+  projectId: "iqtestupload",
+  storageBucket: "iqtestupload.appspot.com",
+  messagingSenderId: "1045353089399",
+  appId: "1:1045353089399:web:e921f8910028d4b91db972",
+  measurementId: "G-Y50EWBBRFQ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const imageDb = getStorage(app);
