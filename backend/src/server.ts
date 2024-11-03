b2f89d5a84f2e28c6fae3fbc3f764966f6589680
Kel Soberano <kellsoberano@gmail.com>
2024-11-03 21:54:21 +0800

    backend - added limit for upload in server.ts/ added all iqtest images

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -12,18 +12,21 @@ dotenv.config();
 
 const app: Application = express();
 
+// Connect to the database
 connectDB();
 
+// Middleware configuration
 app.use(cors());
-app.use(express.json());
+// Increase the JSON and URL-encoded payload limits to 50 MB to handle large requests
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
-//Use routes
-app.use('/api/user16pf', user16PFTestRoutes);   // 16PF test routes
+// Use routes
+app.use('/api/user16pf', user16PFTestRoutes);  // 16PF test routes
 app.use('/api/useriq', userIQTestRoutes);
 app.use('/api/16pf', Test16PFRoutes);
 app.use('/api/IQtest', IQTestRoutes);
-app.use('/api', uploadRoutes);  // Add upload route here
-
+app.use('/api', uploadRoutes);  // Upload route
 
 const PORT = process.env.PORT || 5000;
 
--- student/src/app/Test/Test.tsx
+++ student/src/app/Test/Test.tsx
@@ -4,7 +4,12 @@ import { Link } from 'react-router-dom';
 const Test: React.FC = () => {
     return (
         <div>
+        <div>
             <Link to="/pftest">16PF Test</Link>
+        </div>
+        <div>
+            <Link to="/iqtest">IQ Test</Link>
+        </div>
         </div>
     );
 };
