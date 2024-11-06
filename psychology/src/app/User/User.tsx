95d86a24af1df30f7af639f66cbb8b4bf95a26c2
charlysss-l <charlizemikaelanadela@gmail.com>
2024-11-06 19:55:28 +0800

    Psychology - user table

--- psychology/src/app/User/User.tsx
+++ psychology/src/app/User/User.tsx
@@ -1,8 +1,28 @@
 import React from 'react'
-
+import style from './page.module.scss'
 const User = () => {
   return (
-    <div>User</div>
+    <div>
+    <h2 className={style.userTitle}>List of Users</h2>
+    <table className={style.table}>
+        <thead>
+            <tr>
+                <th className={style.th}>User ID</th>
+                <th className={style.th}>Username</th>
+                <th className={style.th}>Role</th>
+            </tr>
+        </thead>
+        <tbody>
+
+                        <tr>
+                            <td className={style.td}></td>
+                            <td className={style.td}></td>
+                            <td className={style.td}></td>
+                        </tr>
+
+        </tbody>
+    </table>
+</div>
   )
 }
 
