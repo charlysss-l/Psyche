7d4dc1e764ce3229c7ecb0a74ba8807b8bd53122
charlysss-l <charlizemikaelanadela@gmail.com>
2024-11-06 20:06:22 +0800

    Psychology - Profile

--- psychology/src/app/Profile/Profile.tsx
+++ psychology/src/app/Profile/Profile.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from 'react';
+import style from './page.module.scss';
 
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
 
-export default Profile
+
+  return (
+    <div className={style.container}>
+      <h2>User Information</h2>
+      <div className={style.infoContainer}>
+      <label>UserID</label>
+        <input
+          type="userID"
+          name="userID"
+        />
+        <label>Username</label>
+        <input
+          type="text"
+          name="username"
+        />
+        <label>Password</label>
+        <input
+          type="password"
+          name="password"
+        />
+        <div className={style.buttonContainer}>
+        
+            <button  className={style.submitButton}>
+              Submit
+            </button>
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
