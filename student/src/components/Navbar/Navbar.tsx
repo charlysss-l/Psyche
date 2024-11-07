c8a8becead95b0fb9c479251c57b7b0678d8f380
charlysss-l <charlizemikaelanadela@gmail.com>
2024-11-07 08:34:57 +0800

    Student - NAVBAR & SIDEBAR

--- student/src/components/Navbar/Navbar.tsx
+++ student/src/components/Navbar/Navbar.tsx
@@ -1,26 +1,26 @@
 import React from 'react'
-import style from './studentnavbar.module.scss'
+import style from './page.module.scss'
 import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
    <nav className={style.navbar_ps}>
-    <ul>
-        <li>
+    <ul className={style.navbar_UL}>
+        <li className={style.navbar_LI}>
             <Link to="/" className={style.navlink}>Home</Link>
         </li>
-        <li>
+        <li className={style.navbar_LI}>
             <Link to="/test" className={style.navlink}>Test</Link>
         </li>
-        <li>
+        <li className={style.navbar_LI}>
             <Link to="/result" className={style.navlink}>Result</Link>
-        </li>
-        <li>
+        </li >
+        <li className={style.navbar_LI}>
             <Link to="/consultation" className={style.navlink}>Consultation</Link>
         </li>
 
-<li>
-    <Link to="/Login" className={style.navlink}>Login</Link>
-</li>
+        <li className={style.navbar_LI}>
+            <Link to="/Login" className={style.navlink}>Login</Link>
+        </li>
         
     </ul>
 
