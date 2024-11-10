5af54f5cf9a72b942cfe8be2f735fd57f1618418
Tyn <103063597+ajyn06@users.noreply.github.com>
2024-11-11 03:32:36 +0800

    Students - NavBar update

--- student/src/components/Navbar/Navbar.tsx
+++ student/src/components/Navbar/Navbar.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import style from './studentNavbar.module.scss';
+import React from "react";
+import { Link } from "react-router-dom";
+import style from "./studentNavbar.module.scss";
 
 const Navbar = () => {
   return (
@@ -9,23 +9,33 @@ const Navbar = () => {
         <h1>Discover U</h1>
         <p>Student</p>
       </div>
-      
+
       <div className={style.navigationSection}>
         <ul className={style.navList}>
           <li className={style.navItem}>
-            <Link to="/" className={style.navLink}>Home</Link>
+            <Link to="/home" className={style.navLink}>
+              Home
+            </Link>
           </li>
           <li className={style.navItem}>
-            <Link to="/test" className={style.navLink}>Test</Link>
+            <Link to="/test" className={style.navLink}>
+              Test
+            </Link>
           </li>
           <li className={style.navItem}>
-            <Link to="/result" className={style.navLink}>Result</Link>
+            <Link to="/result" className={style.navLink}>
+              Result
+            </Link>
           </li>
           <li className={style.navItem}>
-            <Link to="/consultation" className={style.navLink}>Consultation</Link>
+            <Link to="/consultation" className={style.navLink}>
+              Consultation
+            </Link>
           </li>
           <li className={style.navItem}>
-            <Link to="/login" className={style.navLink}>Login</Link>
+            <Link to="/" className={style.navLink}>
+              Logout
+            </Link>
           </li>
         </ul>
       </div>
