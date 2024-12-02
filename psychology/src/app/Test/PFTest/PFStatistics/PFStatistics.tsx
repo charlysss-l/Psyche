ade5ad684890a98fbb8feec659d7fe347356e06e
Kel Soberano <kellsoberano@gmail.com>
2024-12-02 19:48:39 +0800

    psychology - color meaning

--- psychology/src/app/Test/PFTest/PFStatistics/PFStatistics.tsx
+++ psychology/src/app/Test/PFTest/PFStatistics/PFStatistics.tsx
@@ -166,7 +166,7 @@ const PFStatistics: React.FC = () => {
       {
         label: 'Left Meaning',
         data: filteredResults.map(result => result.left),
-        backgroundColor: 'purple',
+        backgroundColor: 'violet',
       },
       {
         label: 'Average',
