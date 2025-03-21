7f88ea6656497d9a7ae4ed6803a79501f2ef2150
Kel Soberano <kellsoberano@gmail.com>
2025-03-21 13:24:15 +0800

    student - results respoinsive

--- student/src/app/Result/IQResult/IQBothList/IQResultListBoth.tsx
+++ student/src/app/Result/IQResult/IQBothList/IQResultListBoth.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { useEffect } from 'react';
 import IQResultsList from '..//IQOnlineList/IQResultList';
 import OmrIQResultsList from '..//IQOMRList/OmrIQResultList';
 import styles from './IQResultListBoth.module.scss';
 
 const IQResultListBoth: React.FC = () => {
+
+    useEffect(() => {
+                // Set viewport for zoom-out effect
+                const metaViewport = document.querySelector('meta[name="viewport"]');
+                if (metaViewport) {
+                  metaViewport.setAttribute("content", "width=device-width, initial-scale=0.8, maximum-scale=1.0");
+                } else {
+                  const newMeta = document.createElement("meta");
+                  newMeta.name = "viewport";
+                  newMeta.content = "width=device-width, initial-scale=0.8, maximum-scale=1.0, user-scalable=no";
+                  document.head.appendChild(newMeta);
+                }
+            
+                // Cleanup function to reset viewport when leaving the page
+                return () => {
+                  if (metaViewport) {
+                    metaViewport.setAttribute("content", "width=device-width, initial-scale=1.0");
+                  }
+                };
+              }, []);
+
   return (
     <div className={styles.dashboardContainer}>
       <h1 className={styles.dashboardTitle}>Results Dashboard</h1>
--- student/src/app/Result/PFResult/PFResultsDashboard/PFResultsDashboard.tsx
+++ student/src/app/Result/PFResult/PFResultsDashboard/PFResultsDashboard.tsx
@@ -3,6 +3,7 @@ import PFOnlineList from '../PFOnlineList/PFOnlineList'
 import PFOMRList from '../PFOMRList/PFOMRList'
 import style from '../studentpfresult.module.scss'
 const PFResultsDashboard = () => {
+  
   return (
     <div className={style.pfresultDashboard}>
         <h1 >PFResultsDashboard</h1>
