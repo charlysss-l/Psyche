1ad2621bf8ffe56b92fa6658ad07e09ce7948b69
Kel Soberano <kellsoberano@gmail.com>
2025-06-11 10:03:31 +0800

    guidance - sidebar

--- guidance/src/components/Navbar/Navbar.tsx
+++ guidance/src/components/Navbar/Navbar.tsx
@@ -1,17 +1,21 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import style from "./guidancenavbar.module.scss";
-import DarkMode from "../../darkMode/darkMode";
 
 import homeIcon from "../../images/home-page.png";
 import calendarIcon from "../../images/calendar.png";
 import consultationIcon from "../../images/conversation.png";
 import accountIcon from "../../images/user.png";
+import logoImage from "../../images/LOGOnewDark.png";
+import settingIcon from "../../images/setting.png";
+
 
 const Navbar = () => {
   const navigate = useNavigate();
   const role = localStorage.getItem("role");
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isSidebarExpanded, setIsSidebarExpanded] = useState(false);
+  const [isMobileSidebarOpen, setIsMobileSidebarOpen] = useState(false);
+  const sidebarRef = useRef<HTMLDivElement>(null);
   const [counselorTitle, setCounselorTitle] = useState("Counselor");
   const [isEditing, setIsEditing] = useState(false);
   const [startAnimation, setStartAnimation] = useState(false); // Track animation state
@@ -41,89 +45,129 @@ const Navbar = () => {
     navigate("/login");
   };
 
-  const toggleDropdown = () => {
-    setIsDropdownOpen((prevState) => !prevState);
+  const handleIconClick = () => {
+    if (window.innerWidth <= 768) {
+      setIsMobileSidebarOpen(false);
+    } else {
+      setIsSidebarExpanded(true);
+    }
   };
+
+  const handleCloseSidebar = () => {
+    setIsSidebarExpanded(false);
+    setIsMobileSidebarOpen(false);
+  };
+
+    useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (sidebarRef.current && !sidebarRef.current.contains(event.target as Node)) {
+        handleCloseSidebar();
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   const navLinks = [
     { to: "/home", label: "Home", icon: homeIcon },
     { to: "/calendar", label: "Calendar", icon: calendarIcon },
     { to: "/consultation", label: "Consultation", icon: consultationIcon },
     ...(role === "main" ? [{ to: "/create-account", label: "Account", icon: accountIcon }] : []),
+    { to: "/profile", label: "Settings", icon: settingIcon },
+
   ];
 
   return (
-    <nav className={style.studentNavbar}>
-      <div className={style.logoSection}>
-        <h1>DiscoverU</h1>
-        {isEditing ? (
-          <input
-            type="text"
-            value={counselorTitle}
-            onChange={handleTitleChange}
-            onBlur={handleTitleBlur}
-            autoFocus
-            className={style.editableInput}
-          />
-        ) : (
-          <p
-            onClick={() => setIsEditing(true)}
-            className={`${style.editableText} ${startAnimation ? style.animated : ""}`}
-            onMouseEnter={() => setStartAnimation(true)} // Start animation on hover
-          >
-            <span>{counselorTitle}</span>
-          </p>
-        )}
-      </div>
+     <>
+      <button className={style.burgerButton} onClick={() => setIsMobileSidebarOpen(true)}>
+        <span className={style.burgerLines}></span>
+        <span className={style.burgerLines}></span>
+        <span className={style.burgerLines}></span>
+      </button>
 
-      <div className={style.navigationSection}>
-        <ul className={style.navList}>
-          {navLinks.map((link) => (
-            <li className={style.navItem} key={link.to}>
-              <NavLink
-                to={link.to}
-                className={({ isActive }) =>
-                  isActive ? `${style.navLink} ${style.active}` : style.navLink
-                }
-              >
-                <img src={link.icon} alt={`${link.label} icon`} className={style.navIcon} />
-                {link.label}
-              </NavLink>
-            </li>
-          ))}
-        </ul>
-      </div>
+      {isMobileSidebarOpen && (
+        <div className={style.overlay} onClick={handleCloseSidebar}></div>
+      )}
 
-      <div className={style.navRight}>
-        <div className={style.dropdown}>
-          <button className={style.dropdownToggle} onClick={toggleDropdown}>
-            <img
-              src="https://w7.pngwing.com/pngs/340/956/png-transparent-profile-user-icon-computer-icons-user-profile-head-ico-miscellaneous-black-desktop-wallpaper-thumbnail.png"
-              alt="Account"
-              className={style.accountImage}
-            />
-            <span className={style.arrowIcon}>&#9662;</span>
-          </button>
-          {isDropdownOpen && (
-            <ul className={style.dropdownMenu}>
-              <li className={style.dropdownItem}>
-                <NavLink to="/profile" className={style.dropdownLinkSettings}>
-                  Settings
-                </NavLink>
-              </li>
-              <li className={style.dropdownItem}>
-                <DarkMode />
-              </li>
-              <li className={style.dropdownItem}>
-                <button onClick={handleLogout} className={style.dropdownLinkLogout}>
-                  Logout
-                </button>
-              </li>
-            </ul>
+      <nav
+        className={`${style.studentNavbar} ${
+          isMobileSidebarOpen
+            ? style.mobileOpen
+            : isSidebarExpanded
+            ? style.expanded
+            : style.collapsed
+        }`}
+        ref={sidebarRef}
+      >
+        <div className={style.logoSection}>
+          {isSidebarExpanded || isMobileSidebarOpen ? (
+            <>
+              <h1>DiscoverU</h1>
+              {isEditing ? (
+                  <input
+                    type="text"
+                    value={counselorTitle}
+                    onChange={handleTitleChange}
+                    onBlur={handleTitleBlur}
+                    autoFocus
+                    className={style.editableInput}
+                  />
+                ) : (
+                  <p
+                    onClick={() => setIsEditing(true)}
+                    className={`${style.editableText} ${startAnimation ? style.animated : ""}`}
+                    onMouseEnter={() => setStartAnimation(true)} // Start animation on hover
+                  >
+                    <span className={style.editableSpan}>{counselorTitle}</span>
+                  </p>
+                )}
+            </>
+          ) : (
+            <img src={logoImage} alt="DiscoverU Logo" className={style.logoImage} />
           )}
         </div>
-      </div>
-    </nav>
+       
+  <div className={style.navigationSection}>
+          <ul className={style.navList}>
+            {navLinks.map((link) => (
+              <li
+                className={style.navItem}
+                key={link.to}
+                onClick={handleIconClick}
+                data-label={link.label} 
+              >
+                <NavLink
+                  to={link.to}
+                  className={({ isActive }) =>
+                    isActive ? `${style.navLink} ${style.active}` : style.navLink
+                  }
+                >
+                  <img
+                    src={link.icon}
+                    alt={`${link.label} icon`}
+                    className={style.navIcon}
+                  />
+                  {(isSidebarExpanded || isMobileSidebarOpen) && (
+                    <span className={style.span}>{link.label}</span>
+                  )}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        {(isSidebarExpanded || isMobileSidebarOpen) && (
+          <div className={style.navRight}>
+            <button onClick={handleLogout} className={style.logoutButton}>
+              Logout
+            </button>
+          </div>
+        )}
+      </nav>
+    </>
   );
 };
 
